d1297d4f5c5429dc73ebffb61f395e03dcbf368e
azaraelcorpin <azarael.corpin@msugensan.edu.ph>
2024-02-06 09:39:27 +0800

    test button hide

--- ter-fe/src/views/LogIn.vue
+++ ter-fe/src/views/LogIn.vue
@@ -20,7 +20,7 @@
         </svg>
       </div> -->
     </div>
-    <!-- <v-btn @click=" dialog=true" class="mt-5" color="green">Test Here</v-btn> -->
+    <v-btn @click=" dialog=true" class="mt-5" color="green">Test Here</v-btn>
         <v-dialog v-model="dialog" persistent>
           <v-card>
             <v-card-title>
@@ -94,7 +94,7 @@ export default {
       selectedRole: '',
       selectedStudent: '',
       selectedFaculty: '',
-      students: ['princearjen.gabato@msugensan.edu.ph', 'clark.apellado@msugensan.edu.ph', 'salma.maningcara@msugensan.edu.ph','jasmine.limjap@msugensan.edu.ph'],
+      students: ['princearjen.gabato@msugensan.edu.ph', 'juliannesimoun.bizmawi@msugensan.edu.ph', 'salma.maningcara@msugensan.edu.ph','jasmine.limjap@msugensan.edu.ph'],
       faculty: ['jessica.aban@msugensan.edu.ph', 'abdul.silongan@msugensan.edu.ph', 'irene.bernadas@msugensan.edu.ph','daryl.valdez@msugensan.edu.ph'],
       resolve: null
     }
